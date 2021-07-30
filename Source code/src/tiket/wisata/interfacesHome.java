983d16421475c37a8119b58316be01c4af4c38a9
Manuver55 <nugrahaperdana60@gmail.com>
2021-07-30 16:43:45 +0700

    Initial commit

---
+++ Source code/src/tiket/wisata/interfacesHome.java
@@ -0,0 +1,10 @@
+package tiket.wisata;
+
+public interface interfacesHome {
+    public String idTiket(); 
+    public void tampilWisata(String jenis);
+    public void tampilHarga(String jenis);
+    public void getDataRiwayat();
+    public void tampilPengaturan();
+    public void tampilFeedback();
+}
